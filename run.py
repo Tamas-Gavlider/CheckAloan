d26e8eb2d9e8b90e3e4bbdb2cf294df844e09791
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-09-10 12:39:47 +0000

    Created Applicant object, init,summary and make changes function

--- run.py
+++ run.py
@@ -1,3 +1,48 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+welcome_message = "CheckAloan"
+
+class Applicant:
+    """
+    Necessary etails of the applicant based on the loan eligibility will be decided
+    """
+    def __init__(self,first_name,last_name,email,phone,age,marital_status,kids,income,expenses):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone = phone
+        self.age = age
+        self.marital_status = marital_status
+        self.kids = kids
+        self.income = income
+        self.expenses = expenses
+    
+    def summary(self):
+        """
+        Summary of the provided details
+        """
+        return (f"Hello {self.first_name} {self.last_name}!"
+        f"Please check if the details below are correct:\n"
+        f"Email: {self.email}\n"
+        f"Phone number: {self.phone}\n"
+        f"Age: {self.age}\n"
+        f"Marital status: {self.marital_status}\n"
+        f"Kids: {self.kids}\n"
+        f"Monthly income: {self.income}\n"
+        f"Monthly expense: {self.expenses}\n")
+
+    def make_changes(self):
+        """
+        Based on user input changes can be made on details or continue
+        """
+        answer = input("To make any changes on the details above enter c and press enter,else enter s and press enter: ")
+        return answer
+
+
+test = Applicant('tamas','gavlider','2312@321.com',1234567890,35,'married',2,2500,1850)
+test.summary()
+a = test.make_changes()
+if( a[-1] == "c"):
+    print("Change details")
+else: 
+    print("Continue")
+
+            
