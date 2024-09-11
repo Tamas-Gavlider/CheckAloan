18e3a7eadd2aecbde57bc1f145e819ad268fa31d
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-09-11 09:48:53 +0000

    Updated the validation logic in applicant_details function,transformed the welcome message into a function, create score variable

--- run.py
+++ run.py
@@ -1,14 +1,25 @@
-welcome_message = ("Welcome to CheckAloan!\n"
-                   "Where you are not alone!\n")
+def welcome_message():
+    print("-------------------------")
+    print(" Welcome to CheckAloan!\n"
+          " Where you are not alone!")
+    print("-------------------------")
+
 
 # The applicant's email and application status will be stored in database to avoid duplicate requests
 database = {}
+"""
+Based on the provided details the applicant will receive points.
+If the score is high enough the application will be approved, if too low it will be auto rejected.
+"""
+score = 0
+
 
 class Applicant:
     """
     Necessary etails of the applicant based on the loan eligibility will be decided
     """
-    def __init__(self,name,email,phone,age,marital_status,kids,income,expenses,loan_amount,monthly_payment):
+
+    def __init__(self, name, email, phone, age, marital_status, kids, income, expenses, loan_amount, monthly_payment):
         self.name = name
         self.email = email
         self.phone = phone
@@ -19,77 +30,85 @@ class Applicant:
         self.expenses = expenses
         self.loan_amount = loan_amount
         self.monthly_payment = monthly_payment
-    
+
     def summary(self):
         """
         Summary of the provided details
         """
-        return (f"Hello {self.name}!\n"
-        f"Please check if the details below are correct:\n"
-        f"Email: {self.email}\n"
-        f"Phone number: {self.phone}\n"
-        f"Age: {self.age}\n"
-        f"Marital status: {self.marital_status}\n"
-        f"Kids: {self.kids}\n"
-        f"Monthly income: {self.income}\n"
-        f"Monthly expense: {self.expenses}\n")
+        return ("-------------------------------------------"
+                f"Hello {self.name}!\n"
+                f"Please check if the details below are correct:\n"
+                f"Email: {self.email}\n"
+                f"Phone number: {self.phone}\n"
+                f"Age: {self.age}\n"
+                f"Marital status: {self.marital_status}\n"
+                f"Kids: {self.kids}\n"
+                f"Monthly income: {self.income}\n"
+                f"Monthly expense: {self.expenses}\n"
+                f"Loan amount: {self.loan_amount}\n"
+                f"Monthyly payment: {self.monthly_payment}\n"
+                "-------------------------------------------")
 
     def make_changes(self):
         """
         Based on user input changes can be made on details or continue
         """
-        answer = input("To make any changes on the details above enter c and press enter,else enter s and press enter: ")
+        answer = input(
+            "To make any changes on the details above enter c and press enter,else enter s and press enter: ")
         return answer
     
+    def check_score(self):
+        """
+        Calculate the score for the provided details
+        """
+    
     def decision(self):
-        if self.income - self.expenses > monthly_payment*2:
+        if self.income - self.expenses > self.monthly_payment * 2:
             return "approved"
-        elif self.income - self.expenses < monthly_payment*2:
+        elif self.income - self.expenses < self.monthly_payment*2:
             return "the requested amount cannot be approved"
         else:
             return "rejected"
-        
-    
+
     def add_to_database(self):
         """
         Add the applicant's email to database as key and the applications status as value
         """
         database[self.email] = self.decision()
         return database
-    
 
 def applicant_details():
     """
-   Getting the details from the applicant and validate inputs
+    Getting the details from the applicant and validate inputs
     """
     name = input("Please provide your full name: ")
-    while name == "" or len(name) < 4:
+    while name == "" or len(name) < 5:
         name = input("Please enter a valid name: ")
     email = input("Please provide your contact email address: ")
     while "@" not in email or "." not in email:
         email = input("Please enter a valid email address: ")
-    phone = int(input("Please provide your contact phone number: "))
-    while phone > 9999999999 or phone < 1000000000:
-        phone = int(input("Please enter a valid phone number: "))
+    phone = input("Please provide your contact phone number: ")
+    while len(phone) > 10 or len(phone) < 10:
+        phone = input("Please enter a valid phone number: ")
     age = int(input("How old are you?: "))
     while True:
-        if age < 18: 
+        if age < 18:
             print("You cannot apply for a loan. You must be at least 18 years old.")
-            return False
+            break
         elif age > 18 and age < 65:
             break
         else:
             print("Sorry you are too old to apply for a loan.")
-            return False
+            break
     marital_status = input("What is your marital status(Married/Single)?: ")
     while marital_status.capitalize()[0] != "M" and marital_status.capitalize()[0] != "S":
-        marital_status = input("Please enter either married or single:")
+        marital_status = input("Please enter either married or single: ")
     while True:
         try:
             kids = int(input("Number of dependent kids: "))
             break
         except ValueError:
-            print("Please enter a valid number: ")
+            print("Please enter a valid number. ")
     while True:
         try:
             income = int(input("Monthly income: "))
@@ -106,12 +125,16 @@ def applicant_details():
         try:
             loan_amount = int(input("How much money would you like to borrow?: "))
             if loan_amount > 20000:
-                answer = input("Sorry the request amount is too high. The maximum amount is 20000. Would you like to proceed with the max amoutn? y/n :")
+                answer = input("Sorry the request amount is too high. The maximum amount is 20000. Would you like to proceed with the max amount? y/n :")
                 if answer.capitalize()[0] == "N":
-                    return False
+                    loan_amount = 0
+                elif answer.capitalize()[0] == "Y":
+                    loan_amount = 20000
+                    break
                 else:
-                    continue
-            break
+                    print("Sorry, you have entered a wrong character. Enter y or n.")
+            else:
+                break
         except ValueError:
             print("Incorrect data was entered.")
     while True:
@@ -120,18 +143,15 @@ def applicant_details():
             break
         except ValueError:
             print("Incorrect data was entered.")
+
+    return name, email, phone, age, marital_status, kids, income, expense, loan_amount, monthly_payment
         
-        
-    return name,email,phone,age,marital_status,kids,income,expense,loan_amount,monthly_payment
 
 
 
-#user = applicant_details()
-#new_applicant = Applicant(user)
+user = applicant_details()
+new_applicant = Applicant(*user)
 
-#test = Applicant('tamas gavlider','2312@321.com',1234567890,35,'married',2,2500,1850)
-#print(test.summary())
-applicant_details()
+print(new_applicant.summary())
 
-    
-            
+# applicant_details()
